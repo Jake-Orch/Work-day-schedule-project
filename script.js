81d650b18d8a6768abb64696ac690b65fb5b7a4e
Aurelian <123247355+Jake-Orch@users.noreply.github.com>
2023-03-02 11:37:51 +0000

    replace method issue

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+var time = dayjs().format('H');
+var timeSlots = document.body.children[1].children;
+console.log (time);
+console.log (timeSlots);
+
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -14,10 +19,29 @@ $(function () {
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
-  //
+
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
+});
+$(document).ready(function () {
+  $(timeSlots).each(function () {
+    var idVal = $(this).attr('id').replace('hour-', '');
+    console.log (idVal);
+    if (idVal == time) {
+      $(this).addClass('present');
+      $(this).removeClass('past');
+      $(this).removeClass('future');
+    } else if (idVal <= time) {
+      $(this).addClass('past');
+      $(this).removeClass('present');
+      $(this).removeClass('future');
+    } else if (idVal >= time) {
+      $(this).addClass('future');
+      $(this).removeClass('past');
+      $(this).removeClass('present');
+    }
+  });
 });
